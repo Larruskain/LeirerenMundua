ab542fa3a5a740c967102bb9dde06533c72a00e2
Larruskain <elarruscain003@ikasle.ehu.eus>
2025-01-08 12:44:51 +0100

    Updates

---
+++ precache-manifest.758721d5194e4aeafa7d2c693e5c39ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e300bcd7d4e182214aa31f17859b5a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e9e4ff9b4998a6bc04",
+    "url": "/static/css/main.ad2d7cf0.chunk.css"
+  },
+  {
+    "revision": "7af5356316c9e60c0796",
+    "url": "/static/js/2.cf9b79f9.chunk.js"
+  },
+  {
+    "revision": "9836b0e3e9deaac4bd51ae5d8d877206",
+    "url": "/static/js/2.cf9b79f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c89438f0f6fd608254",
+    "url": "/static/js/3.8a71b4f4.chunk.js"
+  },
+  {
+    "revision": "86e9e4ff9b4998a6bc04",
+    "url": "/static/js/main.819c2fde.chunk.js"
+  },
+  {
+    "revision": "0c887c3d85cdfa5aed9e",
+    "url": "/static/js/runtime-main.4aceb6bb.js"
+  },
+  {
+    "revision": "bbe03ed755dc3b7db6e3b9618dd5cdda",
+    "url": "/static/media/logoa.bbe03ed7.png"
+  },
+  {
+    "revision": "22e478a15314ad2eb211ab7ec1187a2e",
+    "url": "/static/media/titleImage.22e478a1.png"
+  }
+]);
